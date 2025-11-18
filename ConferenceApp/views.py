7cb5f5c6130bec8bb1fcade8e9899cbe397286ce
LindaOuer <linda.ouerfelli@gmail.com>
2025-11-18 14:27:52 +0100

    API

--- ConferenceApp/views.py
+++ ConferenceApp/views.py
@@ -1,61 +1,78 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Conference
-from django.views.generic import (
-    ListView,
-    DetailView,
-    UpdateView,
-    CreateView,
-    DeleteView,
-)
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from .forms import ConferenceForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-
+from rest_framework import viewsets
+from .serializers import ConferenceSerializer
 # Create your views here.
-def simple_view(request):
-    return HttpResponse("This is a simple view.")
+class ConferenceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows conferences to be viewed or edited.
+    - list() -> GET /conferences/
+    - retrieve() -> GET /conferences/{id}/
+    - create() -> POST /conferences/
+    - update() -> PUT /conferences/{id}/
+    - partial_update() -> PATCH /conferences/{id}/
+    - destroy() -> DELETE /conferences/{id}/
+    """
+    queryset = Conference.objects.all()
+    serializer_class = ConferenceSerializer
+    
 
 
-def home_view(request):
-    return render(request, "ConferenceApp/home.html")
 
 
-def welcome(request, name):
-    return render(request, "ConferenceApp/welcome.html", {"n": name})
 
 
+
+
+class ConferenceCreateView(LoginRequiredMixin, CreateView):
+    model = Conference
+    fields = "__all__"
+    template_name = 'ConferenceApp/conference_form.html'
+class ConferenceDetailsView(DetailView):
+    model = Conference
+    template_name = 'ConferenceApp/conference_details.html'
+
+def welcome(request):
+    return HttpResponse("<h2>Welcome to the Conference App!</h2>")
+
+def home(request, name):
+    return render(request, 'ConferenceApp/home.html', {'name': name})
+
+# Afficher la liste des conf. à partir de la DB
 def listConferences(request):
     conferences = Conference.objects.all()
-    return render(request, "ConferenceApp/list.html", {"conferences": conferences})
-
-
+    return render(
+        request, 
+        'ConferenceApp/list_conferences.html',{'confs': conferences}
+        )
+    
 class ConferenceListView(ListView):
     model = Conference
-    template_name = "ConferenceApp/list.html"
-    context_object_name = "conferences"
-
-
-class ConferenceDetailView(DetailView):
-    model = Conference
-    context_object_name = "conference"
-
+    template_name = 'ConferenceApp/list_conferences.html'
+    context_object_name = 'conferences'
+    
 
 class ConferenceCreateView(CreateView):
     model = Conference
     # fields = "__all__" or []
-    success_url = reverse_lazy("conference_list_view")
+    success_url = reverse_lazy("conference-listLV")
     form_class = ConferenceForm
 
 
 class ConferenceUpdateView(UpdateView):
     model = Conference
     # fields = "__all__"
-    success_url = reverse_lazy("conference_list_view")
+    success_url = reverse_lazy("conference-listLV")
     form_class = ConferenceForm
 
 
 class ConferenceDeleteView(DeleteView):
     model = Conference
     context_object_name = "conference"
-    success_url = reverse_lazy("conference_list_view")
+    success_url = reverse_lazy("conference-listLV")
--- firstProject/urls.py
+++ firstProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for firstProject project.
+URL configuration for firstproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,12 +14,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from UserApp.views import RegisterView
 from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+from ConferenceApp import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+router = DefaultRouter()
+router.register(r'conferences', views.ConferenceViewSet)
+
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('admin/', admin.site.urls),
-    path('conference/', include('ConferenceApp.urls')),
+    path('API/', include(router.urls)),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("admin/", admin.site.urls),
+    path("conference/", include("ConferenceApp.urls")),
+    path(
+        "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="inscription"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
